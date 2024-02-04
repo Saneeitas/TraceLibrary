df0811ee6c2303f233d4b3dbd60b37da2a597f67
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-02-04 23:38:39 +0100

    refix

--- dashboard.php
+++ dashboard.php
@@ -5,10 +5,7 @@ session_start();
 if (!isset($_SESSION["user"])) {
     header("location: login.php");
 }
-//check if logged in as user
-// if($_SESSION["user"]["role"] == "user"){
-//     header("location: index.php");
-// }
+
 //header links
 require "inc/header.php"; ?>
 
